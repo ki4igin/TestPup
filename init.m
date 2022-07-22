13b5daac3e20dab2782affbfa8326a466c8e446d
artem <ki4igin1992@outlook.com>
2022-07-22 21:04:12 +0300

    рефакториг

--- init.m
+++ init.m
@@ -1,37 +1,30 @@
 disp('Start Init');
 
-% pup_port = find_pup_port();
-% pmes_port = find_pmes_port();
+insys_port = find_insys_port();
 
-% if isempty(pup_port)
-%     disp('Плата ПУП не найдена');
-%     return
-% else
-%     fprintf("Плата ПУП подключена к порту %s\n", pup_port.Port);
-% end
-
-% if isempty(pup_port)
-%     disp('Плата ПУП не найдена');
-%     return
-% else
-%     fprintf("Плата измерений подключена к порту %s\n", pmes_port.Port);
-% end
+if isempty(insys_port)
+    disp('Инсус не найдена');
+else
+    fprintf("Инсус подключена к порту %s\n", insys_port.Port);
+end
 
 disp('Complete Init');
 
 
 %% function
-function pup_port = find_pup_port()
+function insys_port = find_insys_port()
+    disp("Поиск Инсус...");
     baudrate = 115200;
-    ports = serialportlist("available");
+    % ports = serialportlist("available");
+    ports = "COM6";
 
     for port = ports
         fprintf("Попытка подключения к порту %s\n", port);
-        pup_port = serialport(port, baudrate, Timeout = 0.5);
-        pup_port.flush();
+        insys_port = serialport(port, baudrate, Timeout = 5);
+        insys_port.flush();
 
         w = warning('off', 'all');
-        data = pup_port.read(6, "uint8");
+        data = insys_port.readline();
         warning(w);
 
         if ~isempty(data)
@@ -40,30 +33,6 @@ function pup_port = find_pup_port()
 
     end
 
-    pup_port = [];
-
-end
-
-function pmes_port = find_pmes_port()
-    baudrate = 115200;
-    ports = serialportlist("available");
-
-    for port = ports
-        fprintf("Попытка подключения к порту %s\n", port);
-        pmes_port = serialport(port, baudrate, Timeout = 0.5);
-        pmes_port.flush();
-
-        w = warning('off', 'all');
-        pmes_port.write("test", "uint8");
-        data = pmes_port.read(4, "char");
-        warning(w);
-
-        if ~isempty(data) && data == "test"
-            return;
-        end
-
-    end
-
-    pmes_port = [];
+    insys_port = [];
 
 end
--- main.m
+++ main.m
@@ -1,45 +1,90 @@
 init
 
-% Задание режима ПУП
-% pup_port.write(0x11, 1);
+i = 0;
+cnt_old = 0;
+k = 0;
+cnt_new = 0;
+cnt_new_old = 0;
 
-% input("Нажмите Enter для начала теста")
-deg_ref = (0:0.1:359.9)';
-data = deg_ref + rand(length(deg_ref), 1);
-deg_mes = deg_ref * 0;
-deg_delta = deg_ref * 0;
+% hold on
+f = figure;
+tiledlayout(2, 1);
+ax1 = nexttile;
+ax2 = nexttile;
+linkaxes([ax1 ax2], 'x');
+p = plot(ax2, 0, 0, 'o', MarkerFaceColor = 'red');
+p_new_old = plot(ax1, [0 0], [0 0], 'o', MarkerFaceColor = 'red');
+al = animatedline(ax2, 'Color', "red");
+al_new = animatedline(ax1, 'Color', "green");
+al_old = animatedline(ax1, 'Color', "blue");
+% hold off
+txt = text(0, 0, "\leftarrow' num2str(0)");
+txt_new = text(ax1, 0, 0, "\leftarrow' num2str(0)");
+txt_old = text(ax1, 0, 0, "\leftarrow' num2str(0)");
 
-figure
-p = plot(0, 0, 'o', MarkerFaceColor = 'red');
-al = animatedline('Color', [0 .7 .7]);
-txt = text(0, 0, "");
+while true
+    line = insys_port.readline();
+    data = sscanf(line, "Time = %ld , Pulse cnt = %d");
 
-for i = 1:length(deg_ref)
-    % pup_write(0x11, uint32(deg_ref(i) * 10));
-    % mes_port.write("test", 4);
-    % data = mes_port.read(6, "single");
-    % deg_mes(i) = data(2);
-    deg_mes(i) = data(i);
-    deg_delta(i) = deg_mes(i) - deg_ref(i);
+    if isempty(data)
+        continue;
+    end
 
-    x = deg_ref(i);
-    y = deg_delta(i);
+    if ~isvalid(f)
+        break;
+    end
 
-    addpoints(al, x, y);
-    p.XData = x;
-    p.YData = y;
-    txt.Position = [x, y];
-    txt.String = ['\leftarrow' num2str(y)];
+    disp(line);
+
+    i = i + 1;
+    % time_ms = data(1);
+    cnt = data(2);
+
+    if cnt_new == cnt
+        k = 0;
+    end
+
+    if cnt == cnt_old
+        k = k + 1;
+    end
+
+    if k > 2
+        cnt_new_old = cnt_new;
+        cnt_new = cnt;
+    end
+
+    cnt_old = cnt;
+
+    if cnt_new_old == 0
+        continue;
+    end
+
+    delta_cnt = cnt_new - cnt_new_old;
+
+    addpoints(al, i, delta_cnt);
+    addpoints(al_new, i, cnt_new);
+    addpoints(al_old, i, cnt_new_old);
+    p.XData = i;
+    p.YData = delta_cnt;
+    p_new_old.XData = [i i];
+    p_new_old.YData = [cnt_new cnt_new_old];
+    txt.Position = [i, delta_cnt];
+    txt_new.Position = [i, cnt_new];
+    txt_old.Position = [i, cnt_new_old];
+    txt.String = ['\leftarrow' num2str(delta_cnt)];
+    txt_new.String = ['\leftarrow' num2str(cnt_new)];
+    txt_old.String = ['\leftarrow' num2str(cnt_new_old)];
+    xlim([0 (i * 1.3)])
+    % ylim(ax1, [0 (cnt_new * 1.3)])
     drawnow limitrate
-
-    pause(0.01);
-
 end
-
-outdata = [deg_ref, deg_mes, deg_delta];
+ 
+outdata = [cnt_new, cnt_new_old, delta_cnt];
 save('data.mat', 'outdata');
+disp("cnt_new, cnt_old, delta_cnt save to outdata in data.mat");
 
 disp("Press Ctrl+C to exit ;)");
+
 while true
 end
 
